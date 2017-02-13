27f3be9cc2fc35fa0428695c7ef95c246d9a0fa6
Marius van Zundert <marius.vanzundert@webcolorsgroup.com>
2017-02-13 20:25:32 +0100

    Removed unused volume control functions to clean up code

--- widgets/music.lua
+++ widgets/music.lua
@@ -1,9 +1,6 @@
 -- Set up hotkey combinations
 local mashshift = {"cmd", "alt", "shift"}
 
--- Set volume increments
-local volumeIncrement = 5
-
 hs.hotkey.bind(mashshift, 'space', hs.spotify.displayCurrentTrack)
 hs.hotkey.bind(mashshift, 'P',     hs.spotify.play)
 hs.hotkey.bind(mashshift, 'O',     hs.spotify.pause)
@@ -11,6 +8,3 @@ hs.hotkey.bind(mashshift, 'N',     hs.spotify.next)
 hs.hotkey.bind(mashshift, 'I',     hs.spotify.previous)
 
 hs.hotkey.bind(mashshift, 'T', function() hs.alert.show(os.date("%A %b %d, %Y - %I:%M%p"), 4) end)
-
-hs.hotkey.bind(mashshift, ']', function() hs.audiodevice.defaultOutputDevice():setVolume(hs.audiodevice.current().volume + 5) end)
-hs.hotkey.bind(mashshift, '[', function() hs.audiodevice.defaultOutputDevice():setVolume(hs.audiodevice.current().volume - 5) end)
