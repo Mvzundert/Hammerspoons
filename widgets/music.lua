d93419eeacfdd33888574b3374987c1ce2c132de
Marius van Zundert <marius.vanzundert@webcolorsgroup.com>
2017-02-13 21:43:55 +0100

    Removed weird display of time in music widget

--- widgets/music.lua
+++ widgets/music.lua
@@ -6,5 +6,3 @@ hs.hotkey.bind(mashshift, 'P',     hs.spotify.play)
 hs.hotkey.bind(mashshift, 'O',     hs.spotify.pause)
 hs.hotkey.bind(mashshift, 'N',     hs.spotify.next)
 hs.hotkey.bind(mashshift, 'I',     hs.spotify.previous)
-
-hs.hotkey.bind(mashshift, 'T', function() hs.alert.show(os.date("%A %b %d, %Y - %I:%M%p"), 4) end)
