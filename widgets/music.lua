50dab54e379108b28ad4d1ec6eb0bb37b2b0e2ac
Marius van Zundert <marius.vanzundert@webcolorsgroup.com>
2017-02-16 09:15:18 +0100

    Added Itunes controls

--- widgets/music.lua
+++ widgets/music.lua
@@ -1,8 +1,15 @@
 -- Set up hotkey combinations
 local mashshift = {"cmd", "alt", "shift"}
+local funkymash = {"cmd", "ctrl", "shift"}
 
 hs.hotkey.bind(mashshift, 'space', hs.spotify.displayCurrentTrack)
 hs.hotkey.bind(mashshift, 'P',     hs.spotify.play)
 hs.hotkey.bind(mashshift, 'O',     hs.spotify.pause)
 hs.hotkey.bind(mashshift, 'N',     hs.spotify.next)
 hs.hotkey.bind(mashshift, 'I',     hs.spotify.previous)
+
+hs.hotkey.bind(funkymash, 'space', hs.itunes.displayCurrentTrack)
+hs.hotkey.bind(funkymash, 'P',     hs.itunes.play)
+hs.hotkey.bind(funkymash, 'O',     hs.itunes.pause)
+hs.hotkey.bind(funkymash, 'N',     hs.itunes.next)
+hs.hotkey.bind(funkymash, 'I', hs.itunes.previous)
