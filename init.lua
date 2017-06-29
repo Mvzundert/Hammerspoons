6354c1f09a16d9936f6cc80340ac892f01b446fb
Marius van Zundert <marius.vanzundert@webcolorsgroup.com>
2017-06-29 15:55:01 +0200

    Just committing so I can go home

--- init.lua
+++ init.lua
@@ -38,6 +38,44 @@ if privateconf ~= nil then
 end
 
 --[==============[
+Custom PHP like print_r function
+Used for debugging hammerspoon
+--]==============]
+function print_r(t)
+  local print_r_cache = {}
+  local function sub_print_r(t, indent)
+    if (print_r_cache[tostring(t)]) then
+      print(indent .. "*" .. tostring(t))
+    else
+      print_r_cache[tostring(t)] = true
+      if (type(t) == "table") then
+        for pos, val in pairs(t) do
+          if (type(val) == "table") then
+            print(indent .. "[" .. pos .. "] => " .. tostring(t) .. " {")
+            sub_print_r(val, indent .. string.rep(" ", string.len(pos) + 8))
+            print(indent .. string.rep(" ", string.len(pos) + 6) .. "}")
+          elseif (type(val) == "string") then
+            print(indent .. "[" .. pos .. '] => "' .. val .. '"')
+          else
+            print(indent .. "[" .. pos .. "] => " .. tostring(val))
+          end
+        end
+      else
+        print(indent .. tostring(t))
+      end
+    end
+  end
+  if (type(t) == "table") then
+    print(tostring(t) .. " {")
+    sub_print_r(t, " ")
+    print("}")
+  else
+    sub_print_r(t, " ")
+  end
+  print()
+end
+
+--[==============[
 Currently default disabled modules:
   - "modes/indicator",
   - "modes/clipshow",
@@ -50,7 +88,7 @@ Enable them for your own use when needed.
 if not module_list then
     module_list = {
         "widgets/keybindings",
-        "widgets/hotkeys",
+        "widgets/hotkeys",        
         "widgets/hcalendar",
         "widgets/music",
         "widgets/colors",
--- widgets/caffeinate.lua
+++ widgets/caffeinate.lua
@@ -5,30 +5,36 @@ local caffeinateTimeOut = caffeinateSettings.get("hs.caffeinateTimeOut") or nil
 local caffeinateTimestamp = caffeinateSettings.get("hs.caffeinateTimestamp") or nil
 local caffeinateMenuIconOff = "./resources/caffeinateMenuOff.pdf"
 local caffeinateMenuIconOn = "./resources/caffeinateMenuOn.pdf"
+local caffeinateDefaultTimeOut = {
+    seconds = nil,
+    icon = "∞",
+    title = "Indefinitely"
+}
 local caffeinateMenuItems = {
   {
     seconds = 900,
-    string = "15 minutes"
+    title = "15 minutes"
   },
   {
     seconds = 1800,
-    string = "30 minutes"
+    title = "30 minutes"
   },
   {
     seconds = 3600,
-    string = "60 minutes"
+    title = "60 minutes"
   },
   {
     seconds = 5400,
-    string = "90 minutes"
+    title = "90 minutes"
   },
   {
     seconds = 7200,
-    string = "120 minutes"
+    title = "120 minutes"
   },
   {
     seconds = nil,
-    string = "∞ Indefinitely"
+    icon = "∞",
+    title = "Indefinitely"
   }
 }
 -- Set functions
@@ -42,32 +48,37 @@ function caffeinateCheckTimer()
         if not caffeinateMenu then
           caffeinateCreateMenu()
         else
-          local caffeinateLockedUntilTimestamp = (caffeinateTimestamp + caffeinateTimeOut["seconds"]) - currentTime - 3600
-          local caffeinateLockedUntil = os.date(" %H:%M:%S", caffeinateLockedUntilTimestamp)
+          local caffeinateLockedUntilTimestamp = ( caffeinateTimestamp + caffeinateTimeOut["seconds"] ) - currentTime - 3600
+          local caffeinateLockedUntil = os.date(" %H:%M:%S",caffeinateLockedUntilTimestamp)
           caffeinateMenu:setTooltip("Caffeinate: Locked for: "..caffeinateLockedUntil)
           caffeinateMenu:setTitle(caffeinateLockedUntil)
         end
       end
+    else
+        if caffeinateTimeOut["icon"] ~= nil  then
+            caffeinateMenu:setTitle(" "..caffeinateTimeOut["icon"])
+        else
+            caffeinateMenu:setTitle(nil)
+        end
     end
   else
     caffeinateMenu:setTooltip("Caffeinate")
+    caffeinateMenu:setTitle(nil)
   end
 end
 function caffeinateToggle()
   caffeinateUpdateMenuItem(hs.caffeinate.toggle("displayIdle"))
 end
+
 function caffeinateUpdateMenuItem(state)
   if not caffeinateMenu then
     caffeinateCreateMenu()
   end
   if state then
     if not caffeinateTimeOut then
-      caffeinateTimeOut = {
-        seconds = 3600,
-        string = "60 minutes"
-      }
-    end
-    informativeText = "Enabled for "..caffeinateTimeOut["string"]
+      caffeinateTimeOut = caffeinateDefaultTimeOut
+    end    
+    informativeText = "Enabled for "..caffeinateTimeOut["title"]
     caffeinateTimestamp = math.floor(hs.timer.secondsSinceEpoch())
     caffeinateMenu:setTooltip("Caffeinate: "..informativeText)
     caffeinateMenu:setIcon(caffeinateMenuIconOn)
@@ -82,9 +93,12 @@ function caffeinateUpdateMenuItem(state)
   caffeinateSettings.set("hs.caffeinateTimestamp", caffeinateTimestamp)
   caffeinateSettings.set("hs.caffeinateTimeOut", caffeinateTimeOut)
 end
+
 function caffeinateUpdateTimeout(timeout)
+  caffeinateTimestamp = math.floor(hs.timer.secondsSinceEpoch())
+  caffeinateSettings.set("hs.caffeinateTimestamp", caffeinateTimestamp)
   caffeinateTimeOut = timeout
-  caffeinateMenu:setTooltip("Caffeinate: Enabled for "..caffeinateTimeOut["string"])
+  caffeinateMenu:setTooltip("Caffeinate: Enabled for "..caffeinateTimeOut["title"])
   if not caffeinateTimestamp then
     caffeinateToggle()
   else
@@ -92,18 +106,12 @@ function caffeinateUpdateTimeout(timeout)
     caffeinateSettings.set("hs.caffeinateTimeOut", caffeinateTimeOut)
   end
 end
+
 function caffeinateCreateMenu()
-  local currentTime = math.floor(hs.timer.secondsSinceEpoch())
-  local caffeinateLockedUntil = " ∞"
-  if caffeinateTimeOut["seconds"] ~= nil then
-    local caffeinateLockedUntilTimestamp = ( caffeinateTimestamp + caffeinateTimeOut["seconds"] ) - currentTime - 3600
-    local caffeinateLockedUntil = os.date("%H:%M:%S", caffeinateLockedUntilTimestamp)
-  end
   caffeinateMenu = hs.menubar.new(true)
   caffeinateMenu:setTooltip("Caffeinate")
   caffeinateMenu:setIcon(caffeinateMenuIconOff)
   caffeinateMenu:setMenu(caffeinatePopulateMenu)
-  caffeinateMenu:setTitle(caffeinateLockedUntil)
   if caffeinateTimestamp then
     hs.caffeinate.toggle("displayIdle")
     caffeinateMenu:setIcon(caffeinateMenuIconOn)
@@ -116,7 +124,7 @@ caffeinatePopulateMenu = function(key)
     table.insert(
       caffeinateMenuData,
       {
-        title = timeoutItem["string"],
+        title = timeoutItem["title"],
         fn = function() caffeinateUpdateTimeout(timeoutItem) end
       }
     )
@@ -126,14 +134,15 @@ caffeinatePopulateMenu = function(key)
       caffeinateMenuData,
       1,
       {
-        title = "Current: "..caffeinateTimeOut["string"],
+        title = "Current: "..caffeinateTimeOut["title"],
         fn = function() caffeinateToggle() end
       }
     )
     table.insert(caffeinateMenuData, 2, { title = "-" })
-  end    
+  end
   return caffeinateMenuData
 end
+
 -- Create menu item
 caffeinateCreateMenu()
 -- Create timer
